f794d1daab3b9296511aeb22a82d8f9afffbe0cb
sirapart <55588819+sirapart@users.noreply.github.com>
2019-09-20 20:47:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,6 @@ def callback():
     decoded = json.loads(json_line)
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
-    while True:
-   userText = input('')
     if (userText == 'สวัสดี') :
         sendText(user,'สวัสดีครับ')
     elif (userText == 'สบายดีใหม') :
